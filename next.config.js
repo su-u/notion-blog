9ce79038d0a28667360140df83feb9cc468f7b41
su-u <aaaiiiuuu35@gmail.com>
2021-01-22 22:06:06 +0900

    更新

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
-const fs = require('fs')
-const path = require('path')
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 const {
   NOTION_TOKEN,
   BLOG_INDEX_ID,
-} = require('./src/lib/notion/server-constants')
+} = require('./src/lib/notion/server-constants');
 
 try {
-  fs.unlinkSync(path.resolve('.blog_index_data'))
+  fs.unlinkSync(path.resolve('.blog_index_data'));
 } catch (_) {
   /* non fatal */
 }
 try {
-  fs.unlinkSync(path.resolve('.blog_index_data_previews'))
+  fs.unlinkSync(path.resolve('.blog_index_data_previews'));
 } catch (_) {
   /* non fatal */
 }
@@ -20,8 +21,8 @@ const warnOrError =
   process.env.NODE_ENV !== 'production'
     ? console.warn
     : msg => {
-        throw new Error(msg)
-      }
+        throw new Error(msg);
+      };
 
 if (!NOTION_TOKEN) {
   // We aren't able to build or serve images from Notion without the
@@ -29,7 +30,7 @@ if (!NOTION_TOKEN) {
   warnOrError(
     `\nNOTION_TOKEN is missing from env, this will result in an error\n` +
       `Make sure to provide one before starting Next.js`
-  )
+  );
 }
 
 if (!BLOG_INDEX_ID) {
@@ -38,7 +39,7 @@ if (!BLOG_INDEX_ID) {
   warnOrError(
     `\nBLOG_INDEX_ID is missing from env, this will result in an error\n` +
       `Make sure to provide one before starting Next.js`
-  )
+  );
 }
 
 module.exports = {
@@ -46,17 +47,20 @@ module.exports = {
 
   webpack(cfg, { dev, isServer }) {
     // only compile build-rss in production server build
-    if (dev || !isServer) return cfg
+    if (dev || !isServer) return cfg;
 
     // we're in build mode so enable shared caching for Notion data
-    process.env.USE_CACHE = 'true'
+    process.env.USE_CACHE = 'true';
 
-    const originalEntry = cfg.entry
+    const originalEntry = cfg.entry;
     cfg.entry = async () => {
-      const entries = { ...(await originalEntry()) }
-      entries['./scripts/build-rss.js'] = './src/lib/build-rss.ts'
-      return entries
-    }
-    return cfg
+      const entries = { ...(await originalEntry()) };
+      entries['./scripts/build-rss.js'] = './src/lib/build-rss.ts';
+      return entries;
+    };
+    return cfg;
   },
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
